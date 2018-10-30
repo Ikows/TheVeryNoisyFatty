099a6b5f397715522cfe3f0c49fb1c5b2b8288fa
Nicolas Coulaud <coulauddev@gmail.com>
2018-10-30 15:07:02 +0100

    stp

--- public/index.php
+++ public/index.php
@@ -18,49 +18,49 @@
 <body>
 <div class="container-fluid gridHolder">
     <div class="row">
-        <div class="col zones"></div>
-        <div class="col zones"></div>
-        <div class="col zones"></div>
-        <div class="col zones"></div>
-        <div class="col zones"></div>
-        <div class="col zones"></div>
-        <div class="col zones"></div>
+        <div class="col zones" id="40"></div>
+        <div class="col zones" id="41"></div>
+        <div class="col zones" id="42"></div>
+        <div class="col zones" id="43"></div>
+        <div class="col zones" id="44"></div>
+        <div class="col zones" id="45"></div>
+        <div class="col zones" id="46"></div>
     </div>
     <div class="row">
-        <div class="col zones"></div>
-        <div class="col zones"></div>
-        <div class="col zones"></div>
-        <div class="col zones"></div>
-        <div class="col zones"></div>
-        <div class="col zones"></div>
-        <div class="col zones"></div>
+        <div class="col zones" id="30"></div>
+        <div class="col zones" id="31"></div>
+        <div class="col zones" id="32"></div>
+        <div class="col zones" id="33"></div>
+        <div class="col zones" id="34"></div>
+        <div class="col zones" id="35"></div>
+        <div class="col zones" id="36"></div>
     </div>
     <div class="row">
-        <div class="col zones"></div>
-        <div class="col zones"></div>
-        <div class="col zones"></div>
-        <div class="col zones"></div>
-        <div class="col zones"></div>
-        <div class="col zones"></div>
-        <div class="col zones"></div>
+        <div class="col zones" id="20"></div>
+        <div class="col zones" id="21"></div>
+        <div class="col zones" id="22"></div>
+        <div class="col zones" id="23"></div>
+        <div class="col zones" id="24"></div>
+        <div class="col zones" id="25"></div>
+        <div class="col zones" id="26"></div>
     </div>
     <div class="row">
-        <div class="col zones"></div>
-        <div class="col zones"></div>
-        <div class="col zones"></div>
-        <div class="col zones"></div>
-        <div class="col zones"></div>
-        <div class="col zones"></div>
-        <div class="col zones"></div>
+        <div class="col zones" id="10"></div>
+        <div class="col zones" id="11"></div>
+        <div class="col zones" id="12"></div>
+        <div class="col zones" id="13"></div>
+        <div class="col zones" id="14"></div>
+        <div class="col zones" id="15"></div>
+        <div class="col zones" id="16"></div>
     </div>
     <div class="row">
-        <div class="col zones"></div>
-        <div class="col zones"></div>
-        <div class="col zones"></div>
-        <div class="col zones"><img class="perso" id="perso" src="assets/images/bonbonzizi.png" alt=""></div>
-        <div class="col zones"></div>
-        <div class="col zones"></div>
-        <div class="col zones"></div>
+        <div class="col zones" id="0"></div>
+        <div class="col zones" id="1"></div>
+        <div class="col zones" id="2"></div>
+        <div class="col zones" id="3"><img class="perso" id="perso" src="assets/images/bonbonzizi.png" alt=""></div>
+        <div class="col zones" id="4"></div>
+        <div class="col zones" id="5"></div>
+        <div class="col zones" id="6"></div>
     </div>
 
 

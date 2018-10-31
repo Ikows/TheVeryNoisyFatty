0f6c2656ce419740f9feec7a74c1d767e54a9701
Nicolas Coulaud <coulauddev@gmail.com>
2018-10-31 15:21:59 +0100

    zeh

--- public/map1.php
+++ public/map1.php
@@ -87,5 +87,11 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 <script src="assets/js/grille.js"></script>
+<script>
+    $(document).ready(function () {
+        horror = new Audio('assets/sons/horror.mp3')
+        horror.play();
+    })
+</script>
 </body>
 </html>
